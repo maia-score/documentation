00bf634ccf764fcde5f650c0892404de24c52f76
Carlos Rivera <carlos@synx.co>
2021-02-22 10:36:27 -0600

    Deploy website - based on 776c803bc053acc7843c9cbab315be0fb1d6b347

---
+++ 30b7e0b4.02370873.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{78:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hello world!","permalink":"/documentation/blog/hello-world"}]}')}}]);
---
+++ 67391c37.ce71c05b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{79:function(o){o.exports=JSON.parse('{"hello":{"allTagsPath":"/documentation/blog/tags","slug":"hello","name":"hello","count":1,"permalink":"/documentation/blog/tags/hello"}}')}}]);
